bca2126005738ffdd79e60137f1815547edfcbeb
Popov Vladimir <tblkto2@rambler.ru>
2023-06-17 15:46:45 +0300

    inisial commit

---
+++ FligthGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FligthGame
+//
+//  Created by Владимир on 17.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
