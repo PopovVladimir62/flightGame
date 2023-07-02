a0c5450d688afbac1b141fb2b9b1a2db341c3fe8
Popov Vladimir <tblkto2@rambler.ru>
2023-07-02 20:15:33 +0300

    deleting buttons and add tap and pan gesture

--- FligthGame/ViewController.swift
+++ FligthGame/ViewController.swift
@@ -15,54 +15,45 @@ final class ViewController: UIViewController {
     //MARK: - UI elements
     private var plane: UIImageView = {
         let imageVIew = UIImageView()
+        imageVIew.isUserInteractionEnabled = true
         return imageVIew
-    }()
-    
-    private var leftButton: UIButton = {
-        let button = UIButton()
-        return button
-    }()
-    
-    private var rightButton: UIButton = {
-        let button = UIButton()
-        return button
-    }()
-    
-    private var fireButton: UIButton = {
-        let button = UIButton()
-        return button
     }()
     
     //MARK: - lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupVC()
-        setupButtons()
         EnemiesVariety(delegate: self).animatedUFO(for: self, enemy: .plane)
+        makePanGestureForPlane()
+        makeTapGestureForPlane()
     }
     
     //MARK: - private
-    @objc private func actionPlane(_ sender: UIButton) {
-        switch sender {
-        case leftButton:
-            if centerXPlane < .screenWidth / 8 + .planeSize / 2{
-                crashAnimate()
-                return
-            }
-            centerXPlane -= .planeStep
-            updatePlaneXPosition()
-        case rightButton:
-            if centerXPlane > .screenWidth - .screenWidth / 8 - .planeSize / 2 + .planeStep {
-                crashAnimate()
-                return
-            }
-            centerXPlane += .planeStep
-            updatePlaneXPosition()
-        case fireButton:
-            PlaneShot().fire(for: self)
-        default:
-            break
+    
+    @objc private func moveThePlane(_ sender: UIPanGestureRecognizer) {
+        guard let senderView = sender.view else { return }
+        let translation = sender.translation(in: view)
+        senderView.center = CGPoint(x: plane.center.x + translation.x,
+                                    y: plane.center.y)
+        centerXPlane = senderView.center.x
+        if centerXPlane < .screenWidth / 8 + .planeSize / 2 || centerXPlane > .screenWidth - .screenWidth / 8 - .planeSize / 2 + .planeStep {
+            crashAnimate()
         }
+        sender.setTranslation(.zero, in: view)
+    }
+    
+    @objc private func shot() {
+        PlaneShot().fire(for: self)
+    }
+    
+    private func makePanGestureForPlane() {
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(moveThePlane))
+        plane.addGestureRecognizer(panGesture)
+    }
+    
+    private func makeTapGestureForPlane() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(shot))
+        plane.addGestureRecognizer(tapGesture)
     }
     
     private func setupVC() {
@@ -73,45 +64,19 @@ final class ViewController: UIViewController {
         view.addSubview(plane)
     }
     
-    private func setupButtons() {
-        leftButton.frame = CGRect(x: .screenWidth/8, y: .screenHeigth - .buttonSize * 1.5, width: .buttonSize, height: .buttonSize)
-        leftButton.setImage(UIImage(systemName: "arrowshape.left.fill"), for: .normal)
-        leftButton.tintColor = .systemGray
-        leftButton.imageView?.layer.transform = CATransform3DMakeScale(2, 2, 2)
-        leftButton.addTarget(self, action: #selector(actionPlane), for: .touchUpInside)
-        view.addSubview(leftButton)
-        
-        rightButton.frame = CGRect(x: .screenWidth - .screenWidth/8 - .buttonSize, y: .screenHeigth - .buttonSize * 1.5, width: .buttonSize, height: .buttonSize)
-        rightButton.setImage(UIImage(systemName: "arrowshape.right.fill"), for: .normal)
-        rightButton.tintColor = .systemGray
-        rightButton.imageView?.layer.transform = CATransform3DMakeScale(2, 2, 2)
-        rightButton.addTarget(self, action: #selector(actionPlane), for: .touchUpInside)
-        view.addSubview(rightButton)
-        
-        fireButton.frame = CGRect(x: .screenWidth - .buttonSize, y: .screenHeigth - .buttonSize * 2.5, width: .buttonSize, height: .buttonSize)
-        fireButton.setImage(UIImage(systemName: "flame.circle.fill"), for: .normal)
-        fireButton.tintColor = .systemRed
-        fireButton.imageView?.layer.transform = CATransform3DMakeScale(2, 2, 2)
-        fireButton.addTarget(self, action: #selector(actionPlane), for: .touchUpInside)
-        view.addSubview(fireButton)
-    }
-    
     private func crashAnimate() {
-        leftButton.isEnabled = false
-        rightButton.isEnabled = false
         plane.image = UIImage(named: "explosion")
-        Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { timer in
-            self.plane.image = UIImage(named: "plane")
-            self.leftButton.isEnabled = true
-            self.rightButton.isEnabled = true
-            self.plane.center.x = .screenWidth / 2
-            self.centerXPlane = self.plane.center.x
+        if let recognizers = plane.gestureRecognizers {
+            for recognizer in recognizers {
+                plane.removeGestureRecognizer(recognizer)
+            }
         }
-    }
-    
-    private func updatePlaneXPosition() {
-        UIView.animate(withDuration: 0.3) {
-            self.plane.center.x = self.centerXPlane
+        Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { timer in
+            self.makePanGestureForPlane()
+            self.makeTapGestureForPlane()
+            self.plane.image = UIImage(named: "plane")
+//            self.plane.center.x = .screenWidth / 2
+//            self.centerXPlane = self.plane.center.x
         }
     }
 }
